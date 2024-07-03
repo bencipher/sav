48bef04bc14c437dd85f9ba2c23b847fc4d3586c
bcx <femolak@gmail.com>
2024-07-03 06:51:32 +0200

    updates

---
+++ custom_prompts.py
@@ -0,0 +1,24 @@
+from langchain_core.prompts import StringPromptTemplate
+from langchain_core.tools import Tool
+from typing import List
+
+
+class CustomPromptTemplate(StringPromptTemplate):
+    template: str
+    tools: List[Tool]
+    # memory: List[str]
+
+    def format(self, **kwargs) -> str:
+        # memory = kwargs.pop("chat_history")
+        intermediate_steps = kwargs.pop("intermediate_steps")
+        thoughts = ""
+        for action, observation in intermediate_steps:
+            thoughts += action.log
+            thoughts += f"\nObservation: {observation}\nThought: "
+        kwargs["agent_scratchpad"] = thoughts
+        # kwargs["chat_history"] = memory
+        kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
+        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
+        return self.template.format(**kwargs)
+
+
